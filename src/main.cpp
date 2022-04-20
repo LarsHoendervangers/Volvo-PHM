5ba1b0de79aae2aab131c0a543c1918a90dd61b9
larslego <larslego@outlook.com>
2022-04-20 14:26:52 +0200

    Edit: Keypad is now working with MCP23017 I2C expander. Added: MCP23017 library from Rob Tillaart <rob.tillaart@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-#include <Arduino.h>
-#include <Keypad.h>
+// Unavailable pins: 6,7,8,9,10,11
+// Input only: 34, 35, 36, 39
+#include <Wire.h>
+#include "MCP23017.h"
+
+#define SDA 21 // YELLOW
+#define SCL 22 // BLUE
+
+#define I2C_ONE 0x20
+//#define SCAN_DELAY 50
+#define SCAN_DELAY 25
 
 #define KP_NO 'N'
 #define KP_YES 'Y'
@@ -8,67 +17,81 @@
 #define KP_NEXT 'F'
 #define KP_CLEAR 'C'
 #define KP_EMPTY '`'
-
 #define BACKLIGHT_LEDS_PIN 33
 
-const byte rows = 3;
-const byte columns = 7;
-char keys[rows][columns] = {
-  {KP_EMPTY, KP_YES,   KP_PREVIOUS, '1', '4', '7', '*'},
-  {KP_POWER, KP_CLEAR,    '2', '5', '8', '0', KP_EMPTY},
-  {          KP_NO,    KP_NEXT,     '3', '6', '9', '#', KP_EMPTY}  
-  // {KP_EMPTY, KP_YES,   KP_PREVIOUS, '1', '4', '7', '*'},
-  // {KP_EMPTY, KP_POWER, KP_CLEAR,    '2', '5', '8', '0' },
-  // {KP_EMPTY, KP_NO,    KP_NEXT,     '3', '6', '9', '#' }
+#define ROWS 6
+#define COLUMNS 3
+
+char keys[ROWS][COLUMNS] = {
+  { KP_YES,      KP_POWER, KP_NO},
+  { KP_PREVIOUS, KP_CLEAR, KP_NEXT},
+  { '1',         '2',      '3'},
+  { '4',         '5',      '6'},
+  { '7',         '8',      '9'},
+  { '*',         '0',      '#'}
 };
 
-byte rowPins[rows] = { 18, 19, 21 };
-byte columnPins[columns] = { 25, 22, 26, 23, 27, 13, 0 };
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, rows, columns);
-
-// Unavailable pins: 6,7,8,9,10,11
-// Input only: 34, 35, 36, 39
+// Row pins are connected to port B
+byte rowPins[ROWS] = { 8, 9, 10, 11, 12, 13 };
+// Column pins are connected to port A
+byte columnPins[COLUMNS] = { 0, 1, 2 };
+MCP23017 MCP(0x20);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Version 1.003");
+  Serial.println("Started");
+  Wire.begin();
+  MCP.begin();
 
-  keypad.setDebounceTime(50);
+  for (int col = 0; col < COLUMNS; col++) {
+    MCP.pinMode(columnPins[col], INPUT_PULLUP);
+  }
 
-  pinMode(BACKLIGHT_LEDS_PIN, OUTPUT);
-  digitalWrite(BACKLIGHT_LEDS_PIN, HIGH);
+  for (int row = 0; row < ROWS; row++) {
+    MCP.pinMode(rowPins[row], INPUT);
+  }
+}
+
+char scanKeys() {
+  char key = '\0';
+
+  byte rowState = 0;
+  int column = -1;
+  for (int row = 0; row < ROWS; row++) {
+    MCP.pinMode(rowPins[row], OUTPUT);
+    MCP.digitalWrite(rowPins[row], LOW);
+    delay(SCAN_DELAY);
+    for (int col = 0; col < COLUMNS; col++) {
+      if (MCP.digitalRead(columnPins[col]) == LOW) {
+        bitSet(rowState, row);
+        column = col;
+      }
+    }
+    MCP.pinMode(rowPins[row], INPUT);
+  }
+
+  int foundCols = 0;
+  for (int i = 0; i < 8; i++) {
+    if (bitRead(rowState, i)) {
+      foundCols++;
+      if (foundCols == 1) key = keys[i][column];
+    }
+  }
+  
+  return key;
+}
+
+char getKey() {
+  return scanKeys();
 }
 
 void loop() {
-  char key = keypad.getKey();
+  char key = getKey();
 
-  if (key && key) {
-    Serial.println(key);
+  if (key) {
+    Serial.print("Key ");
+    Serial.print(key);
+    Serial.print(" pressed.");
+    Serial.println();
   }
-
-  // if (keypad.getKeys()) {
-  //   String msg = "";
-  //   for (int i = 0; i < LIST_MAX; i++) {
-  //     if (keypad.key[i].stateChanged) {
-  //         switch (keypad.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
-  //             case PRESSED:
-  //             msg = " PRESSED.";
-  //         break;
-  //             case HOLD:
-  //             msg = " HOLD.";
-  //         break;
-  //             case RELEASED:
-  //             msg = " RELEASED.";
-  //         break;
-  //             case IDLE:
-  //             msg = " IDLE.";
-  //         }
-  //         Serial.print("Key ");
-  //         Serial.print(keypad.key[i].kchar);
-  //         Serial.println(msg);
-  //     }
-  //   }
-  //   Serial.println(" ");
-  // }
 }
